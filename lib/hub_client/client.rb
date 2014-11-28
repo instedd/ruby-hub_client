60c009b989c83209da3e980c50d90e163e5b5334
Brian J. Cardiff <bcardiff@manas.com.ar>
2014-11-28 00:01:45 -0300

    fix config loading. expose url and config in client

--- lib/hub_client/client.rb
+++ lib/hub_client/client.rb
@@ -1,9 +1,15 @@
 class HubClient::Client
+  attr_reader :config
+
   def initialize options
     @config = HubClient::Config.new options
   end
 
   def notify connector, event, data
     RestClient.post "#{@config.url}/#{connector}/#{event}?token=#{@config.token}", data
+  end
+
+  def url
+    config.url
   end
 end
--- lib/hub_client/config.rb
+++ lib/hub_client/config.rb
@@ -5,7 +5,7 @@ class HubClient::Config
     config = YAML.load_file("#{Rails.root}/config/hub.yml") rescue {}
     config.merge! options
 
-    @url = config[:url] || "https://hub.instedd.org/callback"
-    @token = config[:token] || raise("An authentication token must be specified in hub.yml")
+    @url = config["url"] || "https://hub.instedd.org/callback"
+    @token = config["token"] || raise("An authentication token must be specified in hub.yml")
   end
 end
