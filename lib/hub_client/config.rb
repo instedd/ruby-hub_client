9b724c66c9a42f75f125dbc931b151388c59c89e
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-07-21 12:35:51 -0300

    Make it compatible with the `config` gem

--- lib/hub_client/config.rb
+++ lib/hub_client/config.rb
@@ -2,7 +2,13 @@ class HubClient::Config
   attr_reader :url, :token, :connector_guid, :enabled
 
   def initialize options
-    config = YAML.load_file("#{Rails.root}/config/hub.yml") rescue {}
+    # Prefer the `config` gem if available and a "hub" section exists
+    if defined?(::Settings) && (hub = Settings.hub)
+      config = hub.to_h
+    else
+      config = YAML.load_file("#{Rails.root}/config/hub.yml") rescue {}
+    end
+
     config.merge! options
 
     @enabled = config.fetch('enabled', true)
